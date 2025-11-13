2db5cba6876e903e266891b421a0ccd1aa7a89d5
ejcsm <ejcsm1@gmail.com>
2025-11-13 14:45:33 +0100

    Versión 2.3

--- Python-EJCSM/src/Ejercicios POO/1 - Instituto/Clases.py
+++ Python-EJCSM/src/Ejercicios POO/1 - Instituto/Clases.py
@@ -1,22 +1,22 @@
 class Alumno:
 
-    def __init__(self, nombre, apellido, edad, listaAsignaturas):
+    def __init__(self, nombre, apellido, edad : int):
         self.nombre = nombre
         self.apellido = apellido
         self.edad = edad
-        listaAsignaturas = []
+        self.listaAsignaturas = []
 
     def __str__(self) -> str:
         return str(f"Alumno: {self.nombre}, {self.apellido}. Edad: {self.edad}")
 
 class Profesor:
 
-    def __init__(self, nombre, apellido, user, password, listaAsignaturas):
+    def __init__(self, nombre, apellido, user, password):
         self.nombre = nombre
         self.apellido = apellido
         self.__user = user
         self.__pasword = password
-        listaAsignaturas = []
+        self.listaAsignaturas = []
 
     def __str__(self) -> str:
         return str(f"Profesor: {self.nombre}, {self.apellido}.")
@@ -28,6 +28,9 @@ class Asignatura:
         self.codigo = codigo
         self.profesor = profesor
         self.listaAlumnos = []
+
+    def __str__(self) -> str:
+        return str(f"Asignatura: {self.nombre}, Código: {self.apellido}, Profesor: {self.profesor}.")
 
     @property
     def setProfesor(self, nombreProfesor):
--- Python-EJCSM/src/Ejercicios POO/1 - Instituto/Main.py
+++ Python-EJCSM/src/Ejercicios POO/1 - Instituto/Main.py
@@ -1,8 +1,17 @@
 from Clases import Alumno, Profesor, Asignatura
+from collections.abc import Iterable
 
 listaAlumnos = []
 listaProfesores = []
-listaAsignaturas = {}
+listaAsignaturas = []
+asignaturas = {}
+
+alumno = Alumno("Sara", "Larroda", 19)
+listaAlumnos.append(alumno)
+profesor = Profesor("Daniel", "Bartolomé", "danibat", "goku123")
+listaProfesores.append(profesor)
+asignatura = Asignatura("ADAT", "1", profesor)
+listaAsignaturas.append(asignatura)
 
 def pedirNombre():
     nombre = input("Introduce el nombre: ")
@@ -11,22 +20,25 @@ def pedirNombre():
 def matricularAlumno():
     nombre = input("Introduce el nombre: ")
     apellido = input("Introduce el apellido: ")
-    edad = input("Introduce la edad: ")
+    edad = int(input("Introduce la edad: "))
     alumno = Alumno(nombre, apellido, edad)
     listaAlumnos.append(alumno)
+    print("Se ha matriculado al alumno: ", alumno)
 
 def buscarAlumno(nombre : str):
     for alumno in listaAlumnos:
         if nombre == alumno.nombre:
             print(alumno)
+            return alumno
     else:
         print("No se ha encontrado a ningún alumno con ese nombre.")
+        return None
 
 def mostrarLista(lista):
-    if isinstance(lista, dict):
-        for k in listaAsignaturas.keys():
-            print(k)
-    elif isinstance(lista, lista.abc.Iterable):
+    if not lista:
+        print("La lista está vacía.")
+        return
+    else:
         for i in lista:
             print(i)
 
@@ -37,74 +49,109 @@ def matricularProfesor():
     contraseña = input("Introduce la contraseña.")
     profesor = Profesor(nombre, apellido, usuario, contraseña)
     listaProfesores.append(profesor)
+    print("Se ha matriculado al profesor: ", profesor)
 
 def buscarProfesor(nombre : str):
     for profesor in listaProfesores:
         if nombre == profesor.nombre:
             print(profesor)
+            return profesor
     else:
         print("No se ha encontrado a ningún profesor con ese nombre.")
+        return None
 
 def crearAsignatura():
     nombre = input("Introduce el nombre: ")
     codigo = input("Introduce el código: ")
+    if not listaProfesores:
+        print("No hay ningún profesor matriculado.")
+        return
+    profesor = None
     while True:
-        profesor = input("Introduce el profesor: ")
-        if profesor not in listaProfesores:
-            print("El profesor no está matriculado.")
+        print("Se le asignará un profesor.")
+        profesor = buscarProfesor(pedirNombre())
+        if profesor is None:
             continue
         else: break
     asignatura = Asignatura(nombre, codigo, profesor)
-    listaAsignaturas[asignatura.codigo] = asignatura.listaAlumnos
+    profesor.listaAsignaturas.append(asignatura)
+    print("Se ha creado la asignatura: ", asignatura)
+    asignaturas[asignatura.codigo] = asignatura.listaAlumnos
 
 def buscarAsignatura(codigo : str):
-    for asignatura in listaAsignaturas.keys():
-        if codigo == asignatura.codigo:
-            print(asignatura)
+    for k, v in asignaturas.items():
+        if k == codigo:
+            print(k, v)
+            return codigo
     else:
         print("No se ha encontrado a ningún asignatura con ese código.")
+        return None
+
+def matricularAlumnoAsignatura():
+    print("Se indicará el nombre del alumno.")
+    alumno = buscarAlumno(pedirNombre())
+    if alumno is None:
+        return
+    print("Se indicará el código de la asignatura.")
+    asignatura = buscarAsignatura(pedirNombre())
+    for k, v in asignaturas.items():
+        if k == asignatura:
+            v.append(alumno)
 
 def pedirNumero():
     while True:
         try:
-            opcion = int(input("Introduce una opción."))
+            opcion = int(input("Introduce una opción: "))
             return opcion
         except:
             print("Tienes que introducir un número.")
 
 def menu():
-    print("0. Salir.")
-    print("1. Matricular un alumno.")
-    print("2. Buscar un alumno.")
-    print("3. Mostrar todos los alumnos.")
-    print("4. Matricular un profesor.")
-    print("5. Buscar un profesor.")
-    print("6. Mostrar todos los profesores.")
-    print("7. Crear asignatura")
-    print("8. Buscar una asignatura por código.")
-    print("9. Mostrar todas las asignaturas.")
-    print("10. Matricular un alumno en una asignatura.")
-    opcion = pedirNumero()
     while True:
+        print("0. Salir.")
+        print("1. Matricular un alumno.")
+        print("2. Buscar un alumno.")
+        print("3. Mostrar todos los alumnos.")
+        print("4. Matricular un profesor.")
+        print("5. Buscar un profesor.")
+        print("6. Mostrar todos los profesores.")
+        print("7. Crear asignatura")
+        print("8. Buscar una asignatura por código.")
+        print("9. Mostrar todas las asignaturas.")
+        print("10. Matricular un alumno en una asignatura.")
+        opcion = pedirNumero()
         match opcion:
             case 0:
                 break
             case 1:
                 matricularAlumno()
+                continue
             case 2:
                 buscarAlumno(pedirNombre())
+                continue
             case 3:
                 mostrarLista(listaAlumnos)
+                continue
             case 4:
                 matricularProfesor()
+                continue
             case 5:
                 buscarProfesor(pedirNombre())
+                continue
             case 6:
                 mostrarLista(listaProfesores)
+                continue
             case 7:
                 crearAsignatura()
+                continue
             case 8:
                 buscarAsignatura(pedirNombre())
+                continue
+            case 9:
+                mostrarLista(listaAsignaturas)
+                continue
+            case 10:
+                matricularAlumnoAsignatura()
             case _:
                 print("Introduce una opción válida.")
                 continue
